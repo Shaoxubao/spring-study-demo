db36f62f3b8c50d3f14f25547a932779d0fcf98e
邵绪宝 <shaoxb@yunjiglobal.com>
2019-09-19 10:59:24 +0800

    格式化代码

--- java-ioc-di-better/src/main/java/com/baoge/factory/AnnotationConfigApplicationContext.java
+++ java-ioc-di-better/src/main/java/com/baoge/factory/AnnotationConfigApplicationContext.java
@@ -45,15 +45,19 @@ public class AnnotationConfigApplicationContext {
     private void scanPkg(final String pkg) {
         // 替换包名中的".",将包结构转换为目录结构
         String pkgDir = pkg.replaceAll("\\.", "/");
+
         // 获取目录结构在类路径中的位置(其中url中封装了具体资源的路径)
         URL url = getClass().getClassLoader().getResource(pkgDir);
+
         // 基于这个路径资源(url),构建一个文件对象
         File file = new File(url.getFile());
+
         // 获取此目录中指定标准(以".class"结尾)的文件
         File[] fs = file.listFiles(new FileFilter() {
             public boolean accept(File file) {
                 // 获取文件名
                 String fName = file.getName();
+
                 // 判断该文件是否为目录，如为目录，递归进一步扫描其内部所有文件
                 if(file.isDirectory()) {
                     scanPkg(pkg + "." + fName);
@@ -70,15 +74,19 @@ public class AnnotationConfigApplicationContext {
         for(File f : fs) {
             // 获取文件名
             String fName = f.getName();
+
             // 获取去除.class之后的文件名
             fName = fName.substring(0, fName.lastIndexOf("."));
+
             // 将名字(类名,通常为大写开头)的第一个字母转换小写(用它作为key存储工厂中)
             String beanId = String.valueOf(fName.charAt(0)).toLowerCase() + fName.substring(1);
+
             // 构建一个类全名(包名.类名)
-            String pkgCls=pkg+"." + fName;
+            String pkgCls = pkg + "." + fName;
             try {
                 // 通过反射构建类对象
                 Class<?> c = Class.forName(pkgCls);
+
                 // 判定这个类上是否有MyComponent注解
                 if(c.isAnnotationPresent(MyComponent.class)) {
                     // 将类对象存储到map容器中
@@ -99,16 +107,21 @@ public class AnnotationConfigApplicationContext {
     private void dependencyInjection() {
         // 获取容器中所有的类定义对象
         Collection<Class<?>> classes = beanDefinitionFactory.values();
+
         // 遍历每一个类对象
         for (Class<?> cls : classes) {
             // 获取类对象的名字全称(包名+类名)
             String clsName = cls.getName();
+
             // 获取类名
             clsName = clsName.substring(clsName.lastIndexOf(".") + 1);
+
             // 将类名(通常为大写开头)的第一个字母转换小写
-            String beanId=String.valueOf(clsName.charAt(0)).toLowerCase() + clsName.substring(1);
+            String beanId = String.valueOf(clsName.charAt(0)).toLowerCase() + clsName.substring(1);
+
             // 获取类中所有的属性
             Field[] fields = cls.getDeclaredFields();
+
             // 遍历每一个属性
             for (Field field : fields) {
                 // 如果这个属性上有MyAutowired注解，进行注入操作
@@ -116,30 +129,38 @@ public class AnnotationConfigApplicationContext {
                     try {
                         // 获取属性名
                         String fieldName = field.getName();
-                        System.out.println("属性名:"+fieldName);
+                        System.out.println("属性名:" + fieldName);
+
                         // 定义为属性注入的bean对象(此对象从容器中获取)
                         Object fieldBean = null;
+
                         // 首先根据属性名从容器中取出对象，如果不为null，则赋值给fieldBean对象
                         if(beanDefinitionFactory.get(fieldName) != null) {
                             fieldBean = getBean(fieldName,field.getType());
                         } else {    // 否则按照属性的类型从容器中取出对象进行注入
                             // 获取属性的类型(包名+类名)
                             String type = field.getType().getName();
+
                             // 截取最后的类名
-                            type = type.substring(type.lastIndexOf(".")+1);
+                            type = type.substring(type.lastIndexOf(".") + 1);
+
                             // 将类名(通常为大写开头)的第一个字母转换小写
-                            String fieldBeanId=String.valueOf(type.charAt(0)).toLowerCase()+type.substring(1);
-                            System.out.println("属性类型ID:"+fieldBeanId);
+                            String fieldBeanId = String.valueOf(type.charAt(0)).toLowerCase() + type.substring(1);
+                            System.out.println("属性类型ID:" + fieldBeanId);
+
                             // 根据转换后的类型beanId，从容器中获取对象并赋值给fieldBean对象
-                            fieldBean = getBean(fieldBeanId,field.getType());
+                            fieldBean = getBean(fieldBeanId, field.getType());
                         }
-                        System.out.println("要为属性注入的值:"+fieldBean);
+                        System.out.println("要为属性注入的值:" + fieldBean);
+
                         // 如果fieldBean对象不为空，则为该属性进行注入
                         if(fieldBean != null) {
                             // 获取此类定义的对象的实例对象
                             Object clsBean = getBean(beanId, cls);
+
                             // 设置此属性可访问
                             field.setAccessible(true);
+
                             // 为该属性注入值
                             field.set(clsBean, fieldBean);
                             System.out.println("注入成功!");
@@ -162,15 +183,19 @@ public class AnnotationConfigApplicationContext {
     public Object doGetBean(String beanId) {
         // 根据传入beanId获取类对象
         Class<?> cls = beanDefinitionFactory.get(beanId);
+
         // 根据类对象获取其定义的注解
         MyComponent annotation = cls.getAnnotation(MyComponent.class);
+
         // 获取注解的scope属性值
         String scope = annotation.scope();
+
         try {
             // 如果scope等于singleton,创建单例对象
             if("singleton".equals(scope) || "".equals(scope)) {
+
                 // 判断容器中是否已有该对象的实例,如果没有,创建一个实例对象放到容器中
-                if(singletonBeanFactory.get(beanId)==null) {
+                if(singletonBeanFactory.get(beanId) == null) {
                     Object instance = cls.newInstance();
                     setFieldValues(cls, instance);
                     singletonBeanFactory.put(beanId, instance);
@@ -207,26 +232,33 @@ public class AnnotationConfigApplicationContext {
      * @param cls 类定义对象
      * @param obj 要为其赋值的实例对象
      */
-    private void setFieldValues(Class<?> cls,Object obj) {
+    private void setFieldValues(Class<?> cls, Object obj) {
         // 获取类中所有的成员属性
         Field[] fields = cls.getDeclaredFields();
         // 遍历所有属性
         for (Field field : fields) {
+
             // 如果此属性有MyValue注解修饰，对其进行操作
             if(field.isAnnotationPresent(MyValue.class)) {
                 // 获取属性名
                 String fieldName = field.getName();
+
                 // 获取注解中的值
                 String value = field.getAnnotation(MyValue.class).value();
+
                 // 获取属性所定义的类型
                 String type = field.getType().getName();
+
                 // 将属性名改为以大写字母开头，如：id改为ID，name改为Name
                 fieldName = String.valueOf(fieldName.charAt(0)).toUpperCase() + fieldName.substring(1);
+
                 // set方法名称，如：setId,setName...
                 String setterName = "set" + fieldName;
+
                 try {
                     // 根据方法名称和参数类型获取对应的set方法对象
                     Method method = cls.getDeclaredMethod(setterName, field.getType());
+
                     // 判断属性类型，如类型不一致，则转换类型后调用set方法为属性赋值
                     if("java.lang.Integer".equals(type) || "int".equals(type)) {
                         int intValue = Integer.valueOf(value);
