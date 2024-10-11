c59d03301a1b939716b9d66fc01d8bf02e6cbf7f
shaoxubao <xubao_shao@163.com>
2024-10-11 15:14:04 +0800

    webservice

--- spring-boot/spring-boot-webservice/server/src/main/java/com/baoge/service/ServerServiceDemo.java
+++ spring-boot/spring-boot-webservice/server/src/main/java/com/baoge/service/ServerServiceDemo.java
@@ -4,6 +4,13 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+/**
+ * webservice的接口
+ * @WebService(
+ * name = "ServerServiceDemo",                  // 暴露服务名称
+ * targetNamespace = "http://service.baoge.com" // 命名空间,一般是接口的包名倒序
+ * )
+ */
 @WebService(name = "ServerServiceDemo", targetNamespace = "http://service.baoge.com")
 public interface ServerServiceDemo {
     @WebMethod
