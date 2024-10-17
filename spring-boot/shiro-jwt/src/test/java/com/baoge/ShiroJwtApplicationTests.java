bc59833c1faeea56cbbc65095f5a382ee2e75a33
shaoxubao <xubao_shao@163.com>
2024-10-17 11:21:20 +0800

    shiro

--- spring-boot/shiro-jwt/src/test/java/com/baoge/ShiroJwtApplicationTests.java
+++ spring-boot/shiro-jwt/src/test/java/com/baoge/ShiroJwtApplicationTests.java
@@ -1,13 +1,25 @@
 package com.baoge;
 
+import com.baoge.entity.User;
+import com.baoge.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShiroJwtApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGet() {
+        User user = userService.getById("1846746177417048065");
+        System.out.println(user);
     }
 
 }
