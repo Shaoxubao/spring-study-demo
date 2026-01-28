7cd29abfdecd8059a15761b62dc58a0eef1d6d9a
shaoxubao <xubao_shao@163.com>
2026-01-28 16:24:39 +0800

    cse配置

--- spring-boot/spring-boot-cse/huawei-provider/src/main/java/com/baoge/controller/ProviderController.java
+++ spring-boot/spring-boot-cse/huawei-provider/src/main/java/com/baoge/controller/ProviderController.java
@@ -1,11 +1,18 @@
 package com.baoge.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProviderController {
+
+  @Value("${server.port}")
+  private String port;
+
+  @Value("${app.name}")
+  private String appName;
 
   @GetMapping("/sayHello")
   public String sayHello(@RequestParam("name") String name) {
@@ -14,6 +21,7 @@ public class ProviderController {
 
   @GetMapping("/test")
   public String test() {
+    System.out.println("port: " + port + ", appName: " + appName);
     return "Hello, This is Spring cloud Huawei + CSE!";
   }
 
